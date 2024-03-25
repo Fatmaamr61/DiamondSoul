71130c6c6d1ab36a1d22c02abfcaff543554b42b
Fatma Amr <fatmaamr@Fatmas-MacBook-Pro.local>
2024-03-25 19:54:22 +0200

    first commit

---
+++ db/models/category.model.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema, model, Types } from "mongoose";
+
+const categorySchema = new Schema(
+  {
+    name: { type: String, required: true, min: 4, max: 20 },
+    slug: { type: String, required: true },
+    image: {
+      url: { type: String, required: true },
+      id: { type: String, required: true },
+    },
+    products: [{ type: Types.ObjectId, ref: "product" }],
+    createdBy: { type: Types.ObjectId, ref: "user", required: true },
+    brandId: [{ type: Types.ObjectId, ref: "brand" }],
+  },
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
+
+export const categoryModel =
+  mongoose.models.categoryModel || model("category", categorySchema);
---
+++ src/appRouter.js
@@ -0,0 +1,69 @@
+/* import authRouter from "./modules/auth/auth.router.js";
+import categoryRouter from "./modules/category/category.router.js";
+import subcategoryRouter from "./modules/subCategory/subCategory.router.js";
+import brandRouter from "./modules/brand/brand.router.js";
+import productRouter from "./modules/product/product.router.js";
+import couponRouter from "./modules/coupon/coupon.router.js";
+import cartRouter from "./modules/cart/cart.router.js";
+import orderRouter from "./modules/order/order.router.js"; */
+import morgan from "morgan";
+import cors from "cors";
+
+export const appRouter = (app, express) => {
+  // morgan
+  if (process.env.NODE_ENV == "dev") {
+    app.use(morgan("dev"));
+  }
+
+  // CORS
+  app.use(cors());
+
+  // global middleware
+/*   app.use((req, res, next) => {
+    // req.originalUrl
+    if (req.originalUrl === "/order/webhook") {
+      return next();
+    }
+    express.json()(req, res, next);
+  });
+ */
+  //routes
+  /* //auth
+  app.use("/auth", authRouter);
+
+  // category
+  app.use("/category", categoryRouter);
+
+  // subCategory
+  app.use("/subcategory", subcategoryRouter);
+
+  // brand
+  app.use("/brand", brandRouter);
+
+  // product
+  app.use("/product", productRouter);
+
+  // coupon
+  app.use("/coupon", couponRouter);
+
+  // cart
+  app.use("/cart", cartRouter);
+
+  // order
+  app.use("/order", orderRouter);
+ */
+  // not found page router
+  app.all("*", (req, res, next) => {
+    return next(new Error("page not found!", { cause: 404 }));
+  });
+
+  // global error handler
+  app.use((error, req, res, next) => {
+    const statusCode = error.cause || 500;
+    return res.status(statusCode).json({
+      success: false,
+      message: error.message,
+      stack: error.stack,
+    });
+  });
+};
