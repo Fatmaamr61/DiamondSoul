6a3843d6db51da31d883846d74e91eb140c9a3e5
fatmaamr61 <fatma.amr6197@gmail.com>
2024-06-19 18:16:05 +0300

    .

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,4 +1,4 @@
-import nodemailer from "nodemailer";
+/* import nodemailer from "nodemailer";
 
 export const sendEmail = async ({ to, subject, html, attachments }) => {
   // sender Info
@@ -22,4 +22,36 @@ export const sendEmail = async ({ to, subject, html, attachments }) => {
   });
 
   return emailInfo.accepted.lenght < 1 ? false : true;
+};
+ */
+
+import nodemailer from "nodemailer";
+
+export const sendEmail = async ({ to, subject, html, attachments }) => {
+  try {
+    // sender Info
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com", // Use Gmail's SMTP server
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.SENDERMAIL,
+        pass: process.env.SENDERPASS,
+      },
+    });
+
+    // receiver Info
+    const emailInfo = await transporter.sendMail({
+      from: ` "DiamondSoul" <${process.env.SENDERMAIL}>`, // sender address
+      to,
+      subject,
+      html,
+      attachments,
+    });
+
+    return emailInfo.accepted.length > 0;
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return false;
+  }
 };
